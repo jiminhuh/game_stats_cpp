ccf39946782dc347c485527d932b4298ce859a0f
Jimin Huh <jiminhuh95@gmail.com>
2020-07-04 17:55:58 -0500

    create repository

---
+++ cmake-build-debug/CMakeFiles/game_stats_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game_stats_cpp.dir/main.cpp.o"
+  "game_stats_cpp"
+  "game_stats_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game_stats_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/game_stats_cpp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for game_stats_cpp.
+# This may be replaced when dependencies are built.
